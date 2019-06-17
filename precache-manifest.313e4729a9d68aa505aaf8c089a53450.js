4431cf6e8dfb7cc7cb3072bb732fd9d61727fbaf
fito <fifodominguez@gmail.com>
2019-06-17 16:58:13 -0600

    Updates

---
+++ precache-manifest.313e4729a9d68aa505aaf8c089a53450.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ebafab9b9fa186b801e2208cc48b473",
+    "url": "/eventosreact/index.html"
+  },
+  {
+    "revision": "5a2e7f575a1e31a14483",
+    "url": "/eventosreact/static/css/main.62cb1aec.chunk.css"
+  },
+  {
+    "revision": "2fa6f1474a2cb3bc340e",
+    "url": "/eventosreact/static/js/2.a8c04701.chunk.js"
+  },
+  {
+    "revision": "5a2e7f575a1e31a14483",
+    "url": "/eventosreact/static/js/main.b61cd7da.chunk.js"
+  },
+  {
+    "revision": "c3543275f1d0788f2642",
+    "url": "/eventosreact/static/js/runtime~main.8473e42b.js"
+  }
+]);
